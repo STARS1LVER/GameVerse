f060227b66cd55648265a6571f88ffbf4f051ebb
STARS1LVER <diegocardenaz11@gmail.com>
2023-11-12 21:04:57 -0500

    Agregando estilos y funciones al error page

--- src/app/game-verse/pages/info-error-game-component/info-error-game-component.component.ts
+++ src/app/game-verse/pages/info-error-game-component/info-error-game-component.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-info-error-game-component',
   templateUrl: './info-error-game-component.component.html',
   styleUrls: ['./info-error-game-component.component.css']
 })
-export class InfoErrorGameComponentComponent {
+export class InfoErrorGameComponentComponent implements OnInit {
+
+  // Properties:
+  seconds: number = 5;
+
+
+  constructor( private router:Router ){}
+  ngOnInit(): void {
+    const intervalodID = setInterval(() => {
+      this.seconds--
+
+      if( this.seconds  === 0){
+        this.router.navigate(['/gameverse/welcome-page'])
+      }
+    },1000)
+  }
+
 
 }
